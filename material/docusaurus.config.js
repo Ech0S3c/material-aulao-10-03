0dccaa6c48411d325bb12edb6a748dd845e9a1be
JvWandermurem <joao.oliveira@sou.inteli.edu.br>
2025-03-10 10:53:25 -0300

    Primeiro commit do Docusaurus

--- material/docusaurus.config.js
+++ material/docusaurus.config.js
@@ -15,15 +15,14 @@ const config = {
   favicon: 'img/logo.svg',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  url: 'https://JvWandermurem.github.io', // Seu usuário do GitHub
+  baseUrl: '/material-aulao-10-03/', // Nome do repositório
+
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Ech0S3c', // Usually your GitHub org/user name.
+  projectName: 'material-aulao-10-03', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
